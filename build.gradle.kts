2ace226e9dd8aee5df182fe50a81d911ef13cf2d
mawngo <sitdownrightnow.dev@gmail.com>
2025-07-14 22:34:51 +0700

    build: add gh action to deploy to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
+import com.vanniktech.maven.publish.JavaLibrary
+import com.vanniktech.maven.publish.JavadocJar
+
 plugins {
     `java-library`
     java
-    `maven-publish`
-    id("me.champeau.mrjar") version "0.1.1"
     idea
-    signing
+    id("me.champeau.mrjar") version "0.1.1"
+    id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
 group = project.findProperty("projectGroup") as String
@@ -14,11 +16,6 @@ multiRelease {
     targetVersions(8, 21)
 }
 
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
-
 allprojects {
     repositories {
         mavenCentral()
@@ -26,44 +23,57 @@ allprojects {
 }
 
 publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            groupId = project.findProperty("projectGroup") as String
-            artifactId = project.findProperty("projectName") as String
-            version = project.findProperty("projectVersion") as String
-            from(components["java"])
-
-            pom {
-                name.set("Batch4j")
-                description.set("Simple batch processing library for java")
-                url.set("https://github.com/mawngo/batch4j")
-
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://opensource.org/licenses/MIT")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("mawngo")
-                        name.set("Nguyen Toan")
-                        email.set("sitdownrightnow.dev@gmail.com")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:git:github.com/mawngo/batch4j.git")
-                    developerConnection.set("scm:git:ssh://github.com/mawngo/batch4j.git")
-                    url.set("https://github.com/mawngo/batch4j")
-                }
-            }
-        }
-    }
     repositories {
         maven {
+            name = "build"
             val releasesRepoUrl = layout.buildDirectory.dir("repos/releases")
             val snapshotsRepoUrl = layout.buildDirectory.dir("repos/snapshots")
             url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl.get() else releasesRepoUrl.get())
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = true)
+    if (project.hasProperty("signing.keyId") || System.getenv("ORG_GRADLE_PROJECT_signingInMemoryKey") != null) {
+        signAllPublications()
+    }
+
+    coordinates(
+        project.findProperty("projectGroup") as String,
+        project.findProperty("projectName") as String,
+        project.findProperty("projectVersion") as String
+    )
+
+    configure(
+        JavaLibrary(
+            javadocJar = JavadocJar.Javadoc(),
+            sourcesJar = true,
+        )
+    )
+
+    pom {
+        name.set("Batch4j")
+        description.set("Simple batch processing library for java")
+        url.set("https://github.com/mawngo/batch4j")
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+        developers {
+            developer {
+                id.set("mawngo")
+                name.set("Nguyen Toan")
+                email.set("sitdownrightnow.dev@gmail.com")
+            }
+        }
+        scm {
+            connection.set("scm:git:git:github.com/mawngo/batch4j.git")
+            developerConnection.set("scm:git:ssh://github.com/mawngo/batch4j.git")
+            url.set("https://github.com/mawngo/batch4j")
         }
     }
 }
@@ -77,10 +87,6 @@ dependencies {
 tasks.named<Jar>("jar") {
     archiveClassifier.set("")
     isEnabled = true
-}
-
-signing {
-    sign(publishing.publications["mavenJava"])
 }
 
 tasks.test {
